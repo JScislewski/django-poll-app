84c45e8e1cbddb73cc7a1aac0e1d4714bd844152
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-10-28 01:55:52 +0100

    Fix models.py
    Move method to another class

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    def was_published_recentry(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     def __str__(self):
         return self.question_text
 
@@ -15,9 +18,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
-    def was_published_recentry(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return self.choice_text
