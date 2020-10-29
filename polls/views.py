d4928915bf00b2fff25deae0a41fe1ab59bd73c1
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-10-29 17:56:27 +0100

    Update views.py
    Update results

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
 from django.urls import reverse
 
 
@@ -21,7 +20,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse("You're looking at the result of question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
